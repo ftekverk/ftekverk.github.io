882a61b344f11ff8c06d52263d56815289a2faaf
Finn <ftekverk@gmail.com>
2022-09-25 20:04:01 -0400

    First Python

---
+++ DHCode/python1.py
@@ -0,0 +1,42 @@
+# 2-1
+message = "This is a message!"
+print(message)
+
+# 2-2
+message = "Message1"
+print(message)
+message = "Message2"
+print(message)
+
+# 2-3
+name = "Ernest Hemingway"
+print("Hello " + name + ", would you like to learn some Python today?")
+
+
+
+# 2-4
+name = "Ernest Hemingway"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+# 2-5
+author = "Ernest Hemingway"
+print(author + ' once said, "The world is a fine place and worth fighting for."')
+
+# 2-6
+# Prints the name of the author and a quote from them
+famous_person = "Ernest Hemingway"
+message = famous_person + ' once said, "The world is a fine place and worth fighting for."'
+print(message)
+
+# 2-7
+# Strips the whitespace from the name
+name = " Ernest Hemingway "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+# 2-10
+# Commented!
